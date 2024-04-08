ad74bfa3d819338f229132920299e9737cefd327
Sasha0pro <sasha0pro@bk.ru>
2024-04-08 15:52:34 +0700

    DELETE

--- src/DTOService.php
+++ src/DTOService.php
@@ -2,18 +2,46 @@
 
 namespace DTOService\Bundle;
 
-use DTOService\Bundle\DependencyInjection\DTOServiceExtension;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class DTOService extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+                ->arrayNode('twitter')
+                    ->children()
+                        ->integerNode('client_id')->end()
+                        ->scalarNode('client_secret')->end()
+                    ->end()
+                 ->end() // twitter
+            ->end()
+        ;
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        $containerConfigurator->import('../config/services.yaml');
+
+        $containerConfigurator->parameters()
+            ->set('acme_hello.phrase', $config['phrase'])
+        ;
+
+        if ($config['scream']) {
+            $containerConfigurator->services()
+                ->get('acme_hello.printer')
+                ->class(ScreamingPrinter::class)
+            ;
+        }
+    }
+
     public function getPath(): string
     {
         return dirname(__DIR__);
     }
 
-    public function getContainerExtension(): ?DTOServiceExtension
-    {
-        return new DTOServiceExtension();
-    }
 }
